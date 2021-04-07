1dc02a635a19382b3a4d176a8555f71f7a391c16
Aji Fatur <ajifatur32@gmail.com>
2021-04-08 06:33:18 +0700

    Update file manager

--- resources/views/admin/file/index.blade.php
+++ resources/views/admin/file/index.blade.php
@@ -1,6 +1,6 @@
 @extends('faturcms::template.admin.main')
 
-@section('title', $kategori->folder_kategori)
+@section('title', 'Materi '.$kategori->folder_kategori)
 
 @section('content')
 
@@ -9,10 +9,10 @@
 
     <!-- Breadcrumb -->
     @include('faturcms::template.admin._breadcrumb', ['breadcrumb' => [
-        'title' => $kategori->folder_kategori,
+        'title' => 'Materi '.$kategori->folder_kategori,
         'items' => [
             ['text' => 'File Manager', 'url' => '#'],
-            ['text' => $kategori->folder_kategori, 'url' => '#'],
+            ['text' => 'Materi '.$kategori->folder_kategori, 'url' => '#'],
         ]
     ]])
     <!-- /Breadcrumb -->
@@ -25,8 +25,20 @@
             <div class="tile">
                 <!-- Tile Title -->
                 <div class="tile-title-w-btn">
+                    <!-- Breadcrumb Direktori -->
+                    <ol class="breadcrumb bg-white p-0 mb-0">
+                        @foreach($breadcrumb as $key=>$data)
+                            @if($key + 1 == count($breadcrumb))
+                            <li class="breadcrumb-item active" aria-current="page">{{ $data->folder_nama == '/' ? 'Home' : $data->folder_nama }}</li>
+                            @else
+                            <li class="breadcrumb-item"><a href="{{ route('admin.filemanager.index', ['kategori' => $kategori->slug_kategori, 'dir' => $data->folder_dir]) }}">{{ $data->folder_nama == '/' ? 'Home' : $data->folder_nama }}</a></li>
+                            @endif
+                        @endforeach
+                    </ol>
+                    <!-- /Breadcrumb Direktori -->
                     <div class="btn-group">
-                        <a href="{{ route('admin.blog.tag.create') }}" class="btn btn-sm btn-theme-1"><i class="fa fa-plus mr-2"></i> Tambah Data</a>
+                        <a href="{{ route('admin.blog.tag.create') }}" class="btn btn-sm btn-theme-1"><i class="fa fa-plus mr-2"></i> Tambah Folder</a>
+                        <a href="{{ route('admin.blog.tag.create') }}" class="btn btn-sm btn-secondary"><i class="fa fa-plus mr-2"></i> Tambah File</a>
                     </div>
                 </div>
                 <!-- /Tile Title -->
@@ -52,7 +64,10 @@
                                 @foreach($folders as $data)
                                 <tr>
                                     <td><input type="checkbox"></td>
-                                    <td><i class="fa fa-folder-open mr-1"></i><a href="#">{{ $data->folder_nama }}</a></td>
+                                    <td>
+                                        <i class="fa fa-folder-open mr-1"></i>
+                                        <a href="{{ route('admin.filemanager.index', ['kategori' => $kategori->slug_kategori, 'dir' => $data->folder_dir]) }}">{{ $data->folder_nama }}</a>
+                                    </td>
                                     <td>{{ $data->folder_voucher }}</td>
                                     <td>
                                         <span class="d-none">{{ $data->folder_up }}</span>
@@ -62,10 +77,31 @@
                                     </td>
                                     <td>
                                         <div class="btn-group">
-                                            <a href="#" class="btn btn-sm btn-success" data-toggle="tooltip" title="Buka"><i class="fa fa-folder-open"></i></a>
-                                            <a href="{{ asset('assets/images/icon/'.$data->folder_icon) }}" class="btn btn-sm btn-info btn-magnify-popup" data-toggle="tooltip" title="Folder Icon"><i class="fa fa-image"></i></a>
+                                            <a href="{{ asset('assets/images/folder/'.$data->folder_icon) }}" class="btn btn-sm btn-info btn-magnify-popup" data-toggle="tooltip" title="Icon"><i class="fa fa-image"></i></a>
+                                            <a href="#" class="btn btn-sm btn-success" data-toggle="tooltip" title="Pindah"><i class="fa fa-arrow-right"></i></a>
                                             <a href="#" class="btn btn-sm btn-warning" data-toggle="tooltip" title="Edit"><i class="fa fa-edit"></i></a>
-                                            <a href="#" class="btn btn-sm btn-danger {{ $data->id_tag == 0 ? 'btn-forbidden' : 'btn-delete' }}" data-id="{{ $data->id_tag }}" data-toggle="tooltip" title="Hapus"><i class="fa fa-trash"></i></a>
+                                            <a href="#" class="btn btn-sm btn-danger btn-delete" data-id="{{ $data->id_folder }}" data-toggle="tooltip" title="Hapus"><i class="fa fa-trash"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforeach
+                                @foreach($files as $data)
+                                <tr>
+                                    <td><input type="checkbox"></td>
+                                    <td><i class="fa fa-file mr-1"></i><a href="#">{{ $data->file_nama }}</a></td>
+                                    <td></td>
+                                    <td>
+                                        <span class="d-none">{{ $data->file_up }}</span>
+                                        {{ date('d/m/Y', strtotime($data->file_up)) }}
+                                        <br>
+                                        <small><i class="fa fa-clock-o mr-1"></i>{{ date('H:i', strtotime($data->file_up)) }} WIB</small>
+                                    </td>
+                                    <td>
+                                        <div class="btn-group">
+                                            <a href="{{ asset('assets/images/file/'.$data->file_thumbnail) }}" class="btn btn-sm btn-info btn-magnify-popup" data-toggle="tooltip" title="Thumbnail"><i class="fa fa-image"></i></a>
+                                            <a href="#" class="btn btn-sm btn-success" data-toggle="tooltip" title="Pindah"><i class="fa fa-arrow-right"></i></a>
+                                            <a href="#" class="btn btn-sm btn-warning" data-toggle="tooltip" title="Edit"><i class="fa fa-edit"></i></a>
+                                            <a href="#" class="btn btn-sm btn-danger btn-delete" data-id="{{ $data->id_file }}" data-toggle="tooltip" title="Hapus"><i class="fa fa-trash"></i></a>
                                         </div>
                                     </td>
                                 </tr>
---
+++ src/Http/Controllers/FolderController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ajifatur\FaturCMS\Http\Controllers;
+
+use Auth;
+use File;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use App\User;
+use Ajifatur\FaturCMS\Models\Files;
+use Ajifatur\FaturCMS\Models\Folder;
+use Ajifatur\FaturCMS\Models\FolderKategori;
+
+// use Ajifatur\FaturCMS\Models\FilePath;
+// use Ajifatur\FaturCMS\Models\FileDetail;
+// use Ajifatur\FaturCMS\Models\FileReader;
+// use Ajifatur\FaturCMS\Models\FileThumbnail;
+// use Ajifatur\FaturCMS\Models\FolderIcon;
+// use Ajifatur\FaturCMS\Models\KategoriMateri;
+
+class FolderController extends Controller
+{
+}

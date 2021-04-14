ab82280b2a890bcb2e3f44a80f8f514ea795f102
Aji Fatur <ajifatur32@gmail.com>
2021-04-15 06:36:38 +0700

    Add fitur sertifikat member, detail file manager member

---
+++ resources/views/member/file/index.blade.php
@@ -0,0 +1,104 @@
+@extends('faturcms::template.member.main')
+
+@php $prefix = $kategori->tipe_kategori == 'ebook' || $kategori->tipe_kategori == 'video'  ? 'Materi' : 'Kumpulan'; @endphp
+
+@section('title', $prefix.' '.$kategori->folder_kategori)
+
+@section('content')
+
+<!-- Main -->
+<main class="app-content">
+
+    <!-- Breadcrumb -->
+    @include('faturcms::template.member._breadcrumb', ['breadcrumb' => [
+        'title' => $prefix.' '.$kategori->folder_kategori,
+        'items' => [
+            ['text' => 'File Manager', 'url' => '#'],
+            ['text' => $prefix.' '.$kategori->folder_kategori, 'url' => '#'],
+        ]
+    ]])
+    <!-- /Breadcrumb -->
+
+    <!-- Row -->
+    <div class="row">
+        <!-- Column -->
+        <div class="col-md-12">
+            <!-- Tile -->
+            <div class="tile">
+                <!-- Tile Title -->
+                <div class="tile-title-w-btn">
+                    <!-- Breadcrumb Direktori -->
+                    <ol class="breadcrumb bg-white p-0 mb-0">
+                        @foreach(file_breadcrumb($directory) as $key=>$data)
+                            @if($key + 1 == count(file_breadcrumb($directory)))
+                            <li class="breadcrumb-item active" aria-current="page">{{ $data->folder_nama == '/' ? 'Home' : $data->folder_nama }}</li>
+                            @else
+                            <li class="breadcrumb-item"><a href="{{ route('member.filemanager.index', ['kategori' => $kategori->slug_kategori, 'dir' => $data->folder_dir]) }}">{{ $data->folder_nama == '/' ? 'Home' : $data->folder_nama }}</a></li>
+                            @endif
+                        @endforeach
+                    </ol>
+                    <!-- /Breadcrumb Direktori -->
+                    <div></div>
+                </div>
+                <!-- /Tile Title -->
+                <!-- Tile Body -->
+                <div class="tile-body">
+                    <!-- Folder -->
+                    <div class="row">
+                        @if(count($folders)>0)
+                            @foreach($folders as $data)
+                                <div class="col-md-3 col-sm-6 mb-3">
+                                    <div class="card">
+                                        <div class="card-body text-center">
+                                            <a href="{{ route('member.filemanager.index', ['kategori' => $kategori->slug_kategori, 'dir' => $data->folder_dir]) }}">
+                                                <img src="{{ image('assets/images/folder/'.$data->folder_icon, 'folder') }}" height="100" style="max-width: 100%;">
+                                            </a>
+                                            <p class="h6 mt-3 mb-0"><a href="{{ route('member.filemanager.index', ['kategori' => $kategori->slug_kategori, 'dir' => $data->folder_dir]) }}">{{ $data->folder_nama }}</a></p>
+                                        </div>
+                                        <div class="card-footer d-flex justify-content-between">
+                                            <span data-toggle="tooltip" title="{{ count_folders($data->id_folder, $data->folder_kategori) }} Folder"><i class="fa fa-folder-open mr-1"></i>{{ count_folders($data->id_folder, $data->folder_kategori) }}</span>
+                                            <span data-toggle="tooltip" title="{{ count_files($data->id_folder, $data->folder_kategori) }} File"><i class="fa fa-file mr-1"></i>{{ count_files($data->id_folder, $data->folder_kategori) }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @endif
+                    </div>
+                    <!-- /Folder -->
+
+                    <!-- File -->
+                    <div class="row">
+                        @if(count($files)>0)
+                            @foreach($files as $data)
+                                <div class="col-md-3 col-sm-6 mb-3">
+                                    <div class="card">
+                                        <a href="{{ $data->tipe_kategori == 'tools' ? asset('assets/tools/'.$data->file_konten) : route('member.file.detail', ['kategori' => $kategori->slug_kategori, 'id' => $data->id_file]) }}">
+                                            <img class="card-img-top" src="{{ image('assets/images/file/'.$data->file_thumbnail, 'file') }}">
+                                        </a>
+                                        <div class="card-body text-center">
+                                            <p class="h6 my-0"><a href="{{ $data->tipe_kategori == 'tools' ? asset('assets/tools/'.$data->file_konten) : route('member.file.detail', ['kategori' => $kategori->slug_kategori, 'id' => $data->id_file]) }}">{{ $data->file_nama }}</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @endif
+                    </div>
+                    <!-- /File -->
+                    
+                    <!-- Empty Result -->
+                    @if(count($folders)<=0 && count($files)<=0)
+                    <div class="alert alert-danger text-center mb-0">Tidak ada data di direktori ini.</div>
+                    @endif
+                    <!-- /Empty Result -->
+                </div>
+                <!-- /Tile Body -->
+            </div>
+            <!-- /Tile -->
+        </div>
+        <!-- /Column -->
+    </div>
+    <!-- /Row -->
+</main>
+<!-- /Main -->
+
+@endsection
--- resources/views/template/member/_sidebar.blade.php
+++ resources/views/template/member/_sidebar.blade.php
@@ -10,7 +10,9 @@
         </div>
       </div> -->
       <ul class="app-menu">
+
         <li><a class="app-menu__item {{ Request::path() == 'member' ? 'active' : '' }}" href="{{ route('member.dashboard') }}"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+
         <li class="treeview {{ strpos(Request::url(), '/member/transaksi') || strpos(Request::url(), '/member/rekening') || strpos(Request::url(), '/member/afiliasi') ? 'is-expanded' : '' }}"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-credit-card"></i><span class="app-menu__label">Afiliasi</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
             <li><a class="treeview-item {{ is_int(strpos(Request::url(), route('member.rekening.index'))) ? 'active' : '' }}" href="{{ route('member.rekening.index') }}"><i class="icon fa fa-circle-o"></i> Rekening Anda</a></li>
@@ -19,5 +21,20 @@
             <li><a class="treeview-item {{ is_int(strpos(Request::url(), route('member.withdrawal.index'))) ? 'active' : '' }}" href="{{ route('member.withdrawal.index') }}"><i class="icon fa fa-circle-o"></i> Withdrawal</a></li>
           </ul>
         </li>
+        
+        <li><a class="app-menu__item {{ is_int(strpos(Request::url(), route('member.filemanager.index', ['kategori' => 'e-learning']))) ? 'active' : '' }}" href="{{ route('member.filemanager.index', ['kategori' => 'e-learning']) }}"><i class="app-menu__icon fa fa-folder-open"></i><span class="app-menu__label">Materi E-Learning</span></a></li>
+        <li><a class="app-menu__item {{ is_int(strpos(Request::url(), route('member.filemanager.index', ['kategori' => 'e-library']))) ? 'active' : '' }}" href="{{ route('member.filemanager.index', ['kategori' => 'e-library']) }}"><i class="app-menu__icon fa fa-folder-open"></i><span class="app-menu__label">Materi E-Library</span></a></li>
+        <li><a class="app-menu__item {{ is_int(strpos(Request::url(), route('member.filemanager.index', ['kategori' => 'e-competence']))) ? 'active' : '' }}" href="{{ route('member.filemanager.index', ['kategori' => 'e-competence']) }}"><i class="app-menu__icon fa fa-folder-open"></i><span class="app-menu__label">Materi E-Competence</span></a></li>
+        <li><a class="app-menu__item {{ is_int(strpos(Request::url(), route('member.filemanager.index', ['kategori' => 'e-course']))) ? 'active' : '' }}" href="{{ route('member.filemanager.index', ['kategori' => 'e-course']) }}"><i class="app-menu__icon fa fa-video-camera"></i><span class="app-menu__label">Materi E-Course</span></a></li>
+        <li><a class="app-menu__item {{ is_int(strpos(Request::url(), route('member.filemanager.index', ['kategori' => 'script']))) ? 'active' : '' }}" href="{{ route('member.filemanager.index', ['kategori' => 'script']) }}"><i class="app-menu__icon fa fa-file-text-o"></i><span class="app-menu__label">Kumpulan Copywriting</span></a></li>
+        <li><a class="app-menu__item {{ is_int(strpos(Request::url(), route('member.filemanager.index', ['kategori' => 'tools']))) ? 'active' : '' }}" href="{{ route('member.filemanager.index', ['kategori' => 'tools']) }}"><i class="app-menu__icon fa fa-wrench"></i><span class="app-menu__label">Kumpulan Tools</span></a></li>
+
+        <li class="treeview {{ is_int(strpos(Request::url(), route('member.sertifikat.trainer.index'))) || is_int(strpos(Request::url(), route('member.sertifikat.peserta.index'))) ? 'is-expanded' : '' }}"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-certificate"></i><span class="app-menu__label">E-Sertifikat</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li><a class="treeview-item {{ is_int(strpos(Request::url(), route('member.sertifikat.trainer.index'))) ? 'active' : '' }}" href="{{ route('member.sertifikat.trainer.index') }}"><i class="icon fa fa-circle-o"></i> Trainer</a></li>
+            <li><a class="treeview-item {{ is_int(strpos(Request::url(), route('member.sertifikat.peserta.index'))) ? 'active' : '' }}" href="{{ route('member.sertifikat.peserta.index') }}"><i class="icon fa fa-circle-o"></i> Peserta</a></li>
+          </ul>
+        </li>
+
       </ul>
     </aside>

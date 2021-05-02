af2b5dd8382f1304ab0dce5ab155bd864a21cabf
Aji Fatur <ajifatur32@gmail.com>
2021-05-02 22:44:43 +0700

    Fix bugs on seeder

--- publishable/seeds/DatabaseSeeder.php
+++ publishable/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
+        $this->call(KategoriSettingSeeder::class);
         $this->call(SettingSeeder::class);
     }
 }

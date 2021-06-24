f76a6c18a2b5fb2e35ea90baa3b5d65cd3af2536
ajifatur <ajifatur2@gmail.com>
2021-06-24 12:09:10 +0700

    Fix bug

--- resources/views/member/dashboard/index.blade.php
+++ resources/views/member/dashboard/index.blade.php
@@ -87,7 +87,12 @@
                         <p class="mb-1">Aktivasi akun Anda dengan melakukan pembayaran sejumlah <del>Rp {{ number_format(setting('site.harga_dicoret'),0,'.','.') }}</del> <strong>Rp {{ number_format($komisi->komisi_aktivasi,0,'.','.') }}</strong> ke rekening berikut:</p>
                         <ol>
                             @foreach($default_rekening as $data)
-                            <li><strong>{{ $data->nama_platform }}</strong> dengan nomor rekening <strong>{{ $data->nomor }}</strong> a/n <strong>{{ $data->atas_nama }}</strong>.</li>
+                            <li>
+                                <strong>{{ $data->nama_platform }}</strong> dengan nomor rekening <strong>{{ $data->nomor }}</strong> a/n <strong>{{ $data->atas_nama }}</strong>.
+                                @if($data->tipe_platform == 1)
+                                Kode transfer bank adalah <strong>{{ $data->kode_platform }}</strong>.
+                                @endif
+                            </li>
                             @endforeach
                         </ol>
                     </div>
